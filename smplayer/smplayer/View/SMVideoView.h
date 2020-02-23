9248b9ff8985096fcc67649f520232fa78d992e8
midoks <midoks@163.com>
2020-02-24 04:02:08 +0800

    up

---
+++ smplayer/smplayer/View/SMVideoView.h
@@ -0,0 +1,17 @@
+//
+//  SMVideoView.h
+//  smplayer
+//
+//  Created by midoks on 2020/2/24.
+//  Copyright © 2020 midoks. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SMVideoView : NSView
+
+@end
+
+NS_ASSUME_NONNULL_END
