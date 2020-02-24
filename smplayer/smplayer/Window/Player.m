e105d91f010547af7872a39ed9067b718822e5d7
midoks <midoks@163.com>
2020-02-24 15:49:35 +0800

    Update Player.m

--- smplayer/smplayer/Window/Player.m
+++ smplayer/smplayer/Window/Player.m
@@ -6,6 +6,15 @@
 //  Copyright © 2020 midoks. All rights reserved.
 //
 
+//AppDelegate.m
+//self.player =[[NSWindowController alloc] initWithWindowNibName:@"Player"];
+//
+//[self.player loadWindow];
+//[self.player.window makeKeyWindow];
+//[self.player.window makeMainWindow];
+//[self.player.window center];
+
+
 #import "Player.h"
 
 @interface Player ()
