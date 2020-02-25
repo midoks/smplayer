b9806c9747fb314e19ebf020261c4337d53d8602
midoks <midoks@163.com>
2020-02-25 19:37:22 +0800

    up

--- smplayer/smplayer/Window/Player.m
+++ smplayer/smplayer/Window/Player.m
@@ -52,16 +52,42 @@
     
     [self initVideo];
     
+//    NSTouchBarTextListTemplate
+    NSButton *list = [[NSButton alloc] initWithFrame:CGRectMake(0, 0, 48, 24)];
+    [list setBezelStyle:NSBezelStyleRegularSquare];
+    [list setImage:[NSImage imageNamed:NSImageNameTouchBarAddTemplate]];
+    [list setBordered:NO];
+    [list setTransparent:YES];
+    [_fragToolbarView addView:list inGravity:NSStackViewGravityLeading];
+    
+    NSButton *list2 = [[NSButton alloc] initWithFrame:CGRectMake(0, 0, 48, 24)];
+    [list2 setBezelStyle:NSBezelStyleRegularSquare];
+    [list2 setImage:[NSImage imageNamed:NSImageNameTouchBarAddTemplate]];
+    [list2 setBordered:NO];
+    [list2 setTransparent:YES];
+    [_fragToolbarView addView:list2 inGravity:NSStackViewGravityCenter];
+    
+    NSButton *list3 = [[NSButton alloc] initWithFrame:CGRectMake(0, 0, 48, 24)];
+    [list3 setBezelStyle:NSBezelStyleRegularSquare];
+    [list3 setImage:[NSImage imageNamed:NSImageNameTouchBarAddTemplate]];
+    [list3 setBordered:NO];
+//    [list3 setsca];
+    [list3 setTransparent:YES];
+    [_fragToolbarView addView:list3 inGravity:NSStackViewGravityTrailing];
+    
+//    _fragToolbarView.frame= CGRectMake(0, 0, 200, 24);
+//    _fragVolumeView.frame = CGRectMake(0, 0, 120, 24);
     // 控制视图
     [_oscTopView addView:_fragVolumeView inGravity:NSStackViewGravityLeading];
     [_oscTopView setVisibilityPriority:NSStackViewVisibilityPriorityDetachOnlyIfNecessary forView:_fragVolumeView];
     
-    [_oscTopView addView:_fragControlView inGravity:NSStackViewGravityLeading];
+    [_oscTopView addView:_fragControlView inGravity:NSStackViewGravityCenter];
     [_oscTopView setVisibilityPriority:NSStackViewVisibilityPriorityDetachOnlyIfNecessary forView:_fragControlView];
     
-    [_oscTopView addView:_fragToolbarView inGravity:NSStackViewGravityLeading];
+    [_oscTopView addView:_fragToolbarView inGravity:NSStackViewGravityTrailing];
     [_oscTopView setVisibilityPriority:NSStackViewVisibilityPriorityDetachOnlyIfNecessary forView:_fragToolbarView];
     
+//    _fragVolumeView.hidden = YES;
     
     // 时间线
     NSRect newFrame = NSMakeRect(_flagTimelineView.frame.origin.x, _flagTimelineView.frame.origin.y, _timeControlView.frame.size.width, _flagTimelineView.frame.size.height);
@@ -76,13 +102,13 @@
     NSLog(@"%@", NSStringFromRect(self.window.contentView.frame) );
     player = [[SMVideoView alloc] initWithFrame:self.window.contentView.frame];
     //    self.window.contentView = player;
-//    [player setTranslatesAutoresizingMaskIntoConstraints:NO];
+    //    [player setTranslatesAutoresizingMaskIntoConstraints:NO];
     [self.window.contentView addSubview:player positioned:NSWindowBelow relativeTo:nil];
     
     
-//    for (NSLayoutConstraint *v in self.window.contentView.constraints) {
-//        NSLog(@"%@",v);
-//    }
+    //    for (NSLayoutConstraint *v in self.window.contentView.constraints) {
+    //        NSLog(@"%@",v);
+    //    }
     
 }
 
