916c22e5fcefa4cb2a27216cd44fa5284be3b1a4
midoks <midoks@163.com>
2020-03-27 20:13:41 +0800

    u

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# smplayer
-Mac simple player
+
+
+<p align="center">
+<img height="256" src="https://github.com/iina/iina/raw/master/iina/Assets.xcassets/AppIcon.appiconset/1024-1.png" />
+</p>
+
+<h1 align="center">smplayer</h1>
+
+<p align="center">smplayer is simple macOS player</p>
+
+<p align=center>
+<a href="https://github.com/iina/iina/releases">Releases</a> ·
+<!-- <a href="https://t.me/IINAUsers">Telegram Group</a> -->
+</p>
+
+
+---
+
--- smplayer/smplayer/Util/SMSubtitleShooter.m
+++ smplayer/smplayer/Util/SMSubtitleShooter.m
@@ -86,153 +86,39 @@ static dispatch_once_t _instance_once;
 -(void)request:(NSURL *)url{
     NSDictionary *reqInfoData = [self hash:url];
     
-    NSLog(@"reqInfoData:%@", reqInfoData );
-    
     [manager POST:apiPath parameters:reqInfoData progress:^(NSProgress * _Nonnull uploadProgress) {
     } success:^(NSURLSessionDataTask * task, id responseObject) {
-        NSArray *tmp = responseObject;
-        
-        NSLog(@"%ld,",[tmp count]);
-        
-        
-        NSDictionary *info =  [responseObject objectAtIndex:0];
-        NSLog(@"%@,",[[[info objectForKey:@"Files"] objectAtIndex:0] objectForKey:@"Link"]);
-        
-        NSString *url = [[[info objectForKey:@"Files"] objectAtIndex:0] objectForKey:@"Link"];
-        
-        [self down2:url];
+        NSArray *data = responseObject;
+        NSUInteger count = [data count];
+
+        for (NSUInteger i=0; i<count; i++) {
+            NSDictionary *info =  [responseObject objectAtIndex:i];
+            NSString *downloadURL = [[[info objectForKey:@"Files"] objectAtIndex:0] objectForKey:@"Link"];
+            [self download:downloadURL path:url index:i+1];
+        }
         
     } failure:^(NSURLSessionDataTask * task, NSError * error) {
         NSLog(@"error:%@",error);
     }];
 }
 
--(void)download:(NSString*)url{
-    AFHTTPSessionManager *ownManager = [AFHTTPSessionManager manager];
-    //    ownManager.requestSerializer = [AFHTTPRequestSerializer serializer];
-        ownManager.responseSerializer = [AFHTTPResponseSerializer serializer];
-    ownManager.responseSerializer.acceptableContentTypes = [[NSSet alloc] initWithObjects:@"application/octet-stream",nil];
-    [ownManager GET:url parameters:nil progress:^(NSProgress * downloadProgress) {
-        
-    } success:^(NSURLSessionDataTask * task, id responseObject) {
-        NSLog(@"ok:%@", responseObject);
-        
-        NSData *data =[responseObject dataUsingEncoding:NSUTF8StringEncoding];
-        NSString * str  =[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-        
-//        NSString * str = [self convertDataToHexStr:responseObject];
-        
-        NSLog(@"ok:%@", str);
-    } failure:^(NSURLSessionDataTask * task, NSError * error) {
-        NSLog(@"ee:%@", error);
-    }];
-}
-
--(void)down2:(NSString *)url{
+-(void)download:(NSString *)url path:(NSURL *)pathURL index:(NSInteger)index{
     NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
     AFURLSessionManager *manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:configuration];
     
-//    manager.requestSerializer.stringEncoding = NSUTF8StringEncoding;
     NSURL *URL = [NSURL URLWithString:url];
     NSURLRequest *request = [NSURLRequest requestWithURL:URL];
-//    [request setValue:@"utf-8" forKey:@"charset"];
-    
-    NSLog(@"%@", [request allHTTPHeaderFields]);
     
     NSURLSessionDownloadTask *downloadTask = [manager downloadTaskWithRequest:request progress:nil destination:^NSURL *(NSURL *targetPath, NSURLResponse *response) {
-        NSLog(@"w:%@",response);
-        NSLog(@"ee:%@:%@",targetPath,[response suggestedFilename]);
-   
-    
-        NSString *fileName = [response suggestedFilename];
-//        fileName = [self replaceUnicode:fileName];
-        fileName = [NSString stringWithFormat:@"[%d]%@", 1, fileName];
-
-        
-        //        NSLog(@"dd:%@", [response textEncodingName]);
-        NSStringEncoding encoding = CFStringConvertEncodingToNSStringEncoding(kCFStringEncodingGB_18030_2000);
-        fileName = [[NSString alloc] initWithCString:fileName.UTF8String encoding:encoding];
-        NSLog(@"pp:%@",fileName);
-//        fileName = [self conv:fileName];
-        
+        NSString *fileName;
+        fileName = [NSString stringWithFormat:@"[%ld]%@", index, pathURL.lastPathComponent];
+  
         NSURL *documentsDirectoryURL = [[NSFileManager defaultManager] URLForDirectory:NSDocumentDirectory inDomain:NSUserDomainMask appropriateForURL:nil create:NO error:nil];
         return [documentsDirectoryURL URLByAppendingPathComponent:fileName];
     } completionHandler:^(NSURLResponse *response, NSURL *filePath, NSError *error) {
         NSLog(@"File downloaded to: %@", filePath);
     }];
     [downloadTask resume];
-}
-
--(void)down3:(NSString *)url{
-    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
-    AFURLSessionManager *manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:configuration];
-    
-    manager.responseSerializer = [[NSSet alloc] initWithObjects:@"application/octet-stream",nil];
-    NSURL *URL = [NSURL URLWithString:url];
-    NSURLRequest *request = [NSURLRequest requestWithURL:URL];
-    
-
-    NSURLSessionDataTask *dataTask=[manager dataTaskWithRequest:request uploadProgress:^(NSProgress * _Nonnull uploadProgress) {
-    
-    } downloadProgress:^(NSProgress * _Nonnull downloadProgress) {
-
-    } completionHandler:^(NSURLResponse * _Nonnull response, id  _Nullable responseObject, NSError * _Nullable error) {
-        if (error) {
-            NSLog(@"Error: %@", error);
-        } else {
-            NSLog(@"%@ %@", response, responseObject);
-        }
-    }];
-    [dataTask resume];
-}
-
--(NSString *)replaceUnicode:(NSString *)unicodeStr {
-    
-    NSString *tempStr1 = [unicodeStr stringByReplacingOccurrencesOfString:@"\\u" withString:@"\\U"];
-    NSString *tempStr2 = [tempStr1 stringByReplacingOccurrencesOfString:@"\"" withString:@"\\\""];
-    NSString *tempStr3 = [[@"\"" stringByAppendingString:tempStr2] stringByAppendingString:@"\""];
-    NSData *tempData = [tempStr3 dataUsingEncoding:NSUTF8StringEncoding];
-    
-    NSString* returnStr = [NSPropertyListSerialization propertyListWithData:tempData
-                                              options:NSPropertyListImmutable
-                                               format:NULL
-                                                error:NULL];
-    return [returnStr stringByReplacingOccurrencesOfString:@"\\r\\n" withString:@"\n"];
-}
-
--(NSString*)conv:(NSString*)keyString{
-    NSMutableString *keyWord = [NSMutableString stringWithFormat:@""];
-    for (int i=0; i<[keyString length]; i++) {
-        NSUInteger location = i;
-        unichar temp = [keyString characterAtIndex:location];
-        
-        NSString *str = [NSString stringWithFormat: @"%C", temp];
-        NSString *str2 = [str stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
-        [keyWord appendString:str2];
-        //        [keyWord appendString:@";"];
-    }
-    return keyWord;
-}
-
--(NSString *)convertDataToHexStr:(NSData *)data{
-    if (!data || [data length] == 0) {
-        return @"";
-    }
-    NSMutableString *string = [[NSMutableString alloc] initWithCapacity:[data length]];
-    
-    [data enumerateByteRangesUsingBlock:^(const void *bytes, NSRange byteRange, BOOL *stop) {
-        unsigned char *dataBytes = (unsigned char*)bytes;
-        for (NSInteger i = 0; i < byteRange.length; i++) {
-            NSString *hexStr = [NSString stringWithFormat:@"%x", (dataBytes[i]) & 0xff];
-            if ([hexStr length] == 2) {
-                [string appendString:hexStr];
-            } else {
-                [string appendFormat:@"0%@", hexStr];
-            }
-        }
-    }];
-    
-    return string;
 }
 
 @end
